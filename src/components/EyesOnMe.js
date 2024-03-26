c7089027798e2a15a1b6e07fd5e0670df46bc6f6
Kelvin Gil <gilkelvin2@gmail.com>
2024-03-26 14:23:09 -0400

    done

--- src/components/EyesOnMe.js
+++ src/components/EyesOnMe.js
@@ -1,1 +1,17 @@
-// Code EyesOnMe Component Here
+import Keypad from "./Keypad";
+
+function EyesOnMe(props){
+
+    function blurEvent(event){
+        console.log('Hey! Eyes on me!')
+    }
+    function focusEvent(event){
+        console.log("Good!")
+    }
+
+
+    return <>
+        <button onBlur={blurEvent} onFocus={focusEvent}>Eyes on me</button>
+    </>
+}
+export default EyesOnMe
--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -1,8 +1,14 @@
 // Code Keypad Component Here
 
 function Keypad (){
+    function changeEvent(event){
+        console.log("Entering password...")
+    }
+
     return (
-        <div></div>
+        <div>
+            <input type="password" onChange={changeEvent} ></input>
+        </div>
     )
 }
 
